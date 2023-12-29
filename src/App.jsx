bcb0f66bd81d25e7802b33bbf151103542b1e4c7
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-12-29 14:09:57 +0530

    setUp completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from './components/Home/Home'
+import Navbar from './components/Navbar/Navbar'
+
+const App = () => {
+  return (
+   <>
+      <Home />
+      <Navbar />
+    </>
+  )
+}
+
+export default App
